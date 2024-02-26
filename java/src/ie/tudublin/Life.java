a6ab3f0bff008fd863352eda4ec3d7a6d6aac47b
TommyTU856 <124066720+TommyTU856@users.noreply.github.com>
2024-02-26 22:45:32 +0000

    getting error telling me to commit or stash

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -3,5 +3,21 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Life extends PApplet{
-    
+
+    LifeBoard lifeBoard;
+
+    public void setup() {
+        lifeBoard = new LifeBoard(10, 10, this);
+        lifeBoard.randomize();
+    }
+
+    public void settings() {
+        size (500, 500);
+    }
+
+    public void draw() {
+        background(0);
+        lifeBoard.update();
+        lifeBoard.render();
+    }
 }
---
+++ java/src/ie/tudublin/LifeBoard.java
@@ -0,0 +1,79 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class LifeBoard {
+    
+    boolean [][] board;
+
+    int rows;
+    int cols;
+
+    float cellWidth;
+    float cellHeight;
+    PApplet p;
+
+    public LifeBoard(int rows, int cols, PApplet p) {
+        
+        this.rows = rows;
+        this.cols = cols;
+        this.p = p;
+        board = new boolean[rows][cols];
+        cellWidth = p.width / (float)cols;
+        cellHeight = p.height / (float)rows;
+    }
+
+    void randomize() {
+
+        for(int row = 0; row < rows; row++)
+        {
+            for(int col = 0; col < cols; col++) {
+                
+                float dice = p.random(1.0f);
+                board[row][col] = (dice < 0.5f);
+
+                
+            }
+        }
+    }
+
+    void update() {
+
+    }
+
+    public void setCell(int row, int col, boolean value) {
+        if(row > 0 && col > 0 && row < rows && col < cols) {
+            
+            board[row][col] = value;
+        }
+    }
+
+    public boolean getCell(int row, int col) {
+        if(row > 0 && col > 0 && row < rows && col < cols) {
+            
+            return board[row][col];
+        }
+        return false;
+    }
+
+    public void render() {
+
+        for(int row = 0; row < rows; row++)
+        {
+            for(int col = 0; col < cols; col++) {
+
+                float x = p.map(col, 0, cols, 0, p.width);
+                float y = row * cellHeight;
+
+                if(board[row][col]) {
+                    p.fill(0, 255, 0);
+                } else {
+                    p.noFill();
+                }
+
+                p.rect(x, y, cellWidth, cellHeight);
+            }
+        }
+    }
+
+}
